7abd26c0a0e75e2d4b941e6633c14fe712668317
sai6855 <60743144+sai6855@users.noreply.github.com>
2023-01-09 23:51:43 +0530

    added premier time

--- newtab.js
+++ newtab.js
@@ -1,36 +1,59 @@
- const colors = ["red", "blue", "voilet", "yellow", "green"];
+const colors = ["red", "blue", "voilet", "yellow", "green"];
 
-    document.body.style.backgroundColor = colors[Math.floor(Math.random() * 5)];
+document.body.style.backgroundColor = colors[Math.floor(Math.random() * 5)];
 
-    const anchor_link = document.getElementsByTagName("a")[0];
-    const image = document.getElementsByTagName("img")[0];
+const anchor_link = document.getElementsByTagName("a")[0];
+const image = document.getElementsByTagName("img")[0];
+const premier_text = document.getElementsByClassName("premier-text")[0];
 
-    const base_url = "http://localhost:1337";
+const base_url = "http://localhost:1337";
 
-    const fetch_youtube_premier_url = () => {
-      fetch(base_url + "/api/youtube-premier-urls")
-        .then((res) => res.json())
-        .then((res) => {
-          const url = res.data[0].attributes.url;
-          anchor_link.href = url;
-        });
-    };
-    fetch_youtube_premier_url();
-    setInterval(() => {
-      fetch_youtube_premier_url();
-    }, 1000);
+const fetch_youtube_premier_url = () => {
+  fetch(base_url + "/api/youtube-premier-urls")
+    .then((res) => res.json())
+    .then((res) => {
+      const url = res.data[0].attributes.url;
+      anchor_link.href = url;
+      const day = new Date(res.data[0].attributes.premier_time);
+      const yyyy = day.getFullYear();
+      let mm = day.getMonth() + 1; // Months start at 0!
+      let dd = day.getDate();
+      let hours = day.getHours();
+      let minutues = day.getMinutes();
+      let sec = day.getSeconds();
 
-    const fetch_background_image = () => {
-      fetch(base_url + "/api/new-tab-hero-images?populate=%2A")
-        .then((res) => res.json())
-        .then((res) => {
-          const url =
-            res.data[0].attributes.image.data.attributes.formats.thumbnail.url;
-          image.src = base_url + url;
-          image.style.visibility = "visible";
-        });
-    };
-    fetch_background_image();
-    setInterval(() => {
-      fetch_background_image();
-    }, 1000);
+      if (dd < 10) dd = "0" + dd;
+      if (mm < 10) mm = "0" + mm;
+      if (hours < 10) hours = "0" + hours;
+      if (minutues < 10) minutues = "0" + minutues;
+      if (sec < 10) sec = "0" + sec;
+
+      const formattedToday = dd + "/" + mm + "/" + yyyy;
+      premier_text.innerHTML =
+        day.getTime() > new Date().getTime()
+          ? "Premiering on: " +
+            formattedToday +
+            " " +
+            [hours, minutues, sec].join(":")
+          : "";
+    });
+};
+fetch_youtube_premier_url();
+setInterval(() => {
+  fetch_youtube_premier_url();
+}, 1000);
+
+const fetch_background_image = () => {
+  fetch(base_url + "/api/new-tab-hero-images?populate=%2A")
+    .then((res) => res.json())
+    .then((res) => {
+      const url =
+        res.data[0].attributes.image.data.attributes.formats.thumbnail.url;
+      image.src = base_url + url;
+      image.style.visibility = "visible";
+    });
+};
+fetch_background_image();
+setInterval(() => {
+  fetch_background_image();
+}, 1000);
